a5566c2d827a1d57fefade977aa12c073121a2d9
Ronnie Ross <62971289+ronnieross711@users.noreply.github.com>
2021-04-26 11:10:54 -0500

    setup complete

---
+++ calenderapi/src/Components/ContentContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ContentContainer(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default ContentContainer;
---
+++ calenderapi/src/Components/Searchbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Searchbar(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Searchbar;
